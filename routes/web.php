0cc92084eb9e5f8fccd1523fb87c6f27df88384b
Fraser Provan <FraserProvan2@gmail.com>
2018-12-09 16:19:02 +0000

    removed early testing files

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,6 @@
 | Web Routes
 |------------------------------------------------------------------------*/
 
-// Testing
-// Route::get('/save_list', 'PartPickerAlphaController@save_list');
-// Route::get('/view_list/{id}/', 'PartPickerAlphaController@view_list');
-// Route::get('/view_list/store/{id}/', 'PartPickerAlphaController@view_list');
-// Route::get('/view_list/delete/{id}/', 'PartPickerAlphaController@delete_list');
-
 // Home
 Route::get('/', 'controller@index');
 
