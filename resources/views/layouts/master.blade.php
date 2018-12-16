1a5c6a7a69f0c79805ba055f68262ff30148ad40
Fraser Provan <FraserProvan2@gmail.com>
2018-12-16 18:47:25 +0000

    login nolonger in user dropdown, login page now exists

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -70,69 +70,8 @@
                             </form>
 
                         @else
-                            <!-- Log In Form -->
-                            <form method="POST" action="{{ route('login') }}">
-                                @csrf
-        
-                                <div class="form-group row">
-                                    <label for="email" class="col-sm-12 col-form-label">{{ __('E-Mail Address') }}</label>
-        
-                                    <div class="col-md-12">
-                                        <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-        
-                                        @if ($errors->has('email'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('email') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-                                </div>
-        
-                                <div class="form-group row">
-                                    <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
-        
-                                    <div class="col-md-12">
-                                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-        
-                                        @if ($errors->has('password'))
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $errors->first('password') }}</strong>
-                                            </span>
-                                        @endif
-                                    </div>
-                                </div>
-        
-                                <div class="form-group row">
-                                    <div class="col-md-12 offset-md-12">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-        
-                                            <label class="form-check-label" for="remember">
-                                                {{ __('Remember Me') }}
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-        
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-12 offset-md-12">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Login') }}
-                                        </button>
-
-                                        <a href="/register" class="btn btn-secondary">
-                                            Register
-                                        </a>
-                                        
-                                        @if (Route::has('password.request'))
-                                            <a class="btn btn-link pl-none" href="{{ route('password.request') }}">
-                                            Forgot Your Password?
-                                            </a>
-                                        @endif
-                                    </div>
-                                </div>
-                            </form>
-            
+                            <a href="/login" class="dropdown-item has-icon"><i data-feather="user"></i>Login</a>
+                            <a href="/register" class="dropdown-item has-icon"><i data-feather="user"></i>Register</a>
                         @endif
 
                 </div>
@@ -141,7 +80,7 @@
     
             <!-- Cart dropdown -->
             <li class="nav-item dropdown dropdown-hover dropdown-cart">
-            <a class="nav-link nav-icon mr-nis dropdown-toggle forwardable ml-2" data-toggle="dropdown" href="cart.html" role="button" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link nav-icon mr-nis dropdown-toggle forwardable ml-2" data-toggle="dropdown" href="cart" role="button" aria-haspopup="true" aria-expanded="false">
                 <i data-feather="shopping-cart"></i>
                 <span class="badge badge-primary">4</span>
             </a>
@@ -171,7 +110,7 @@
                 </div>
                 <div class="d-flex justify-content-between pb-2">
                 <div class="w-100 mr-1">
-                    <a href="cart.html" class="btn btn-block rounded-pill btn-secondary">View Cart</a>
+                    <a href="/cart" class="btn btn-block rounded-pill btn-secondary">View Cart</a>
                 </div>
                 <div class="w-100 ml-1">
                     <a href="shipping.html" class="btn btn-block rounded-pill btn-primary">Checkout</a>
