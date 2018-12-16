0384bc4e2034c5033f62797f2d9e23f69b573cbd
Fraser Provan <FraserProvan2@gmail.com>
2018-12-16 21:12:28 +0000

    styling for headings login/register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header h5 bold">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
