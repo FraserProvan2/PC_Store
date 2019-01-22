64b4b0e97cf87da10b916d9a45db35d13ebd9542
Fraser Provan <FraserProvan2@gmail.com>
2019-01-22 20:58:55 +0000

    styling to order succes

--- resources/views/public/checkout/order-success.blade.php
+++ resources/views/public/checkout/order-success.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
 
-<div class="card">
-    <div class="card-body">
-      <h2 class="bold text-center mb-4">Payment Success!</h2>
-      <div class="row">
-          <div class="col-md-2"></div>
-          <div class="col-md-4 text-center mb-2">
-                <h4 class="bold">Cart Items</h4>
-
+<h2 class="bold text-center mb-4">Payment Success!</h2>
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header bold h4">Cart Items</div>
+            <div class="card-body">
+                
                 @foreach($cart as $key=>$item)
                     <div class="mb-2 p">
                         @if($item['type'] == 'build')
@@ -33,11 +33,17 @@
                     @endif
                 </div>
 
-                <h4 class="mt-2 bold">Total: <span class="text-primary">£{{ number_format($order_data->price, 2) }}</span></h4>
+                <hr>
 
-          </div>
-          <div class="col-md-4 text-center mb-2">
-                    <h4 class="bold">Details</h4>
+                <h4 class="mt-2 bold">Total: <span class="text-primary">£{{ number_format($order_data->price, 2) }}</span></h4>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-header bold h4">Order Details</div>
+            <div class="card-body">
                     Card Used: 
                     <span>**** **** **** 
                         <span class="text-primary">{{ $card }}</span>
@@ -49,9 +55,9 @@
                     {{ $shipping->number }}<br>
                     {{ $shipping->address }}, {{ $shipping->postcode }}<br>
                     {{ $shipping->country }}, {{ $shipping->city }}<br><br>
-          </div>
-      </div>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
 
 @endsection
