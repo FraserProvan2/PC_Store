e0267b7405cd5aac111e4a1990ccc0bb2efbd3ad
Fraser Provan <FraserProvan2@gmail.com>
2019-01-23 00:40:06 +0000

    order number in payment success

--- resources/views/public/checkout/order-success.blade.php
+++ resources/views/public/checkout/order-success.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<h2 class="bold text-center mb-4">Payment Success! <i class="fa fa-check text-primary" aria-hidden="true"></i></h2>
+<h2 class="bold text-center mb-4 mt-4">Payment Success! <i class="fa fa-check text-primary" aria-hidden="true"></i></h2>
 <div class="row">
     <div class="col-md-2"></div>
     <div class="col-md-4">
@@ -44,6 +44,8 @@
         <div class="card">
             <div class="card-header bold h4">Order Details</div>
             <div class="card-body">
+                    
+                    Order Number: #{{ $order_data->id }}<br>
                     Card Used: 
                     <span>**** **** **** 
                         <span class="text-primary">{{ $card }}</span>
