2201000f63e2df06bea3b9ed3a9c02d566571608
Fraser Provan <FraserProvan2@gmail.com>
2019-01-27 16:26:28 +0000

    homepage additions

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -51,4 +51,70 @@
     
     <div class="container mt-3">
 
+            <div class="row categories-grid gutters-3 mt-gutter">
+
+                    <div class="col-sm-6 col-md-4">
+                      <a href="{{ url('build') }}">
+                        <div class="card zoom-hover">
+                          <div data-cover="{{ asset('img/nzxt.png') }}" data-xs-height="150px" data-sm-height="272px" data-md-height="246px" data-lg-height="324px" data-xl-height="400px" style="background-image: url(&quot;../img/discover/1.jpeg&quot;); height: 324px;"></div>
+                          <div class="card-img-overlay card-img-overlay-transparent">
+                            <h2 class="card-title bold">Build a Custom Gaming PC</h2>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+            
+                    <div class="col-sm-6 col-md-4">
+                      <div class="row gutters-3">
+                        <div class="col-6 col-sm-12 mt-3 mt-sm-0">
+                          <a href="{{ url('components/gpu') }}">
+                            <div class="card zoom-hover">
+                              <div data-cover="{{ asset('img/part-img/gpu-2080ti.jpg') }}" data-xs-height="100px" data-sm-height="128px" data-md-height="115px" data-lg-height="154px" data-xl-height="192px" style="background-image: url(&quot;../img/discover/2.jpeg&quot;); height: 154px;"></div>
+                              <div class="card-img-overlay card-img-overlay-transparent">
+                                <h3 class="card-title text-center">Graphics Cards</h3>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                        <div class="col-6 col-sm-12 mt-3">
+                          <a href="{{ url('components/case') }}">
+                            <div class="card zoom-hover">
+                              <div data-cover="{{ asset('img/part-img/case-dukase.jpg') }}" data-xs-height="100px" data-sm-height="128px" data-md-height="115px" data-lg-height="154px" data-xl-height="192px" style="background-image: url(&quot;../img/discover/3.jpeg&quot;); height: 154px;"></div>
+                              <div class="card-img-overlay card-img-overlay-transparent">
+                                <h3 class="card-title text-center">Cases</h3>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+            
+                    <div class="col-md-4">
+                      <div class="row gutters-3">
+                        <div class="col-6 col-md-12 mt-3 mt-md-0">
+                          <a href="{{ url('components/cpu') }}">
+                            <div class="card zoom-hover">
+                              <div data-cover="{{ asset('img/part-img/cpu-i5.jpg') }}" data-xs-height="100px" data-sm-height="128px" data-md-height="115px" data-lg-height="154px" data-xl-height="192px" style="background-image: url(&quot;../img/discover/4.jpeg&quot;); height: 154px;"></div>
+                              <div class="card-img-overlay card-img-overlay-transparent">
+                                <h2 class="card-title text-center">Processors</h2>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                        <div class="col-6 col-md-12 mt-3">
+                          <a href="{{ url('components/motherboard') }}">
+                            <div class="card zoom-hover">
+                              <div data-cover="{{ asset('img/part-img/mobo-strix.jpg') }}" data-xs-height="100px" data-sm-height="128px" data-md-height="115px" data-lg-height="154px" data-xl-height="192px" style="background-image: url(&quot;../img/discover/5.jpeg&quot;); height: 154px;"></div>
+                              <div class="card-img-overlay card-img-overlay-transparent">
+                                <h2 class="card-title text-center">Motherboards</h2>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+            
+                    <a href="{{ url('components') }}" class="btn btn btn-light shadow-sm sa-btn">View All <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>
+                  </div>
+
 @endsection
